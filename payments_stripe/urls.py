a65c8024b6aaa39c51d3381b8886de8eee026d0e
aya-136 <145042635+aya-136@users.noreply.github.com>
2025-10-13 11:38:12 +0300

    Update urls.py
    payment detail url

--- payments_stripe/urls.py
+++ payments_stripe/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('process/', views.process_payment, name='process_payment'),
     path('create-payment-intent/', views.create_payment_intent, name='create_payment_intent'),
+    path('payment-method-details/', views.payment_method_details, name='payment_method_details'),
 ]
